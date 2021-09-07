4fe6933556e375d9ed2b17bac5f67466fa650d98
yuhung1206 <78803926+yuhung1206@users.noreply.github.com>
2021-09-07 22:47:16 +0800

    Update HS_BW.m

--- HS_BW.m
+++ HS_BW.m
@@ -2,7 +2,9 @@
 clear all; close all; clc;
 flowerImg = imread('camellia (mono) 512x512.tif'); %load
 [R,C] = size(flowerImg)
-% compute original PDF & CDF
+
+
+%-----compute original PDF & CDF-----%(1)
 edge_vector = [0:256]
 figure();
 temp = histogram(flowerImg, edge_vector)
@@ -16,11 +18,14 @@ for i = 1:length(PDF)
     count = count + PDF(i)
     CDF(i) = count;
 end
-% compute specific PDF & CDF
+
+
+%-----compute specific PDF & CDF-----%(2)
 PDF_s = ones(1,length(PDF));
 PDF_s(1,1:64) = PDF_s(1,1:64).*1248;
 PDF_s(1,193:256) = PDF_s(1,193:256).*1248;
 PDF_s(1,65:192) = PDF_s(1,65:192).*800;
+
 % draw picture of histogram
 figure(); bar([0:255],PDF_s);
 title("Desired histogram");
@@ -33,7 +38,9 @@ for i = 1:length(PDF_s)
     count = count + PDF_s(i)
     CDF_s(i) = count;
 end
-% comput Transfer label
+
+
+%-----compute Transfer label-----%(3)
 it_n = 1;
 for i = 1:length(CDF)
     % minimize n for (v_n-s_k)>=0
@@ -46,6 +53,9 @@ for i = 1:length(CDF)
     end
 end
 TransLabel = TransLabel - 1; % shift back to 0~255
+
+
+%-----produce output data-----%(4)
 NewImg = zeros(R,C)
 for i = 1:R
     for j = 1:C
@@ -56,6 +66,8 @@ end
 figure();  temp = histogram(NewImg, edge_vector)
 title("Output Histogram");
 xlabel('gray level(0~255)');  ylabel('number of pixels');
-saveas(gcf,'Output histogram.jpg');
-imwrite(uint8(NewImg), 'OutputImage.jpeg');  close all;
+saveas(gcf,'Output histogram.jpg'); 
 
+
+%-----Export new Image-----%(5)
+imwrite(uint8(NewImg), 'OutputImage.jpeg');  close all;
