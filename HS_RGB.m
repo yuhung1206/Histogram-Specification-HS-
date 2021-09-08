86d67f299f63eb8f519d70ca8fff615b7f9cb3ef
yuhung1206 <78803926+yuhung1206@users.noreply.github.com>
2021-09-08 16:17:44 +0800

    Update HS_RGB.m

--- HS_RGB.m
+++ HS_RGB.m
@@ -4,10 +4,10 @@ clear all; close all; clc;
 ImageFile = 'test.jpg';
 Img = imread(ImageFile);  % load file
 
-% Process to each color layer [R,G,B] (1)~(4)
+% Process to each color layer [R,G,B] step(1)~(4)
 [Img(:,:,1)] = HS_func( Img(:,:,1) );
 [Img(:,:,2)] = HS_func( Img(:,:,2) );
 [Img(:,:,3)] = HS_func( Img(:,:,3) );
 
-%-----Export new Image-----%(5)
+%-----Export new Image-----% step(5)
 imwrite(uint8(Img), 'OutputImageX.jpeg');  close all;
